91ad774a0ac5759ef404e9af38678b41f8362cd7
Sahilwagh142 <sahil.wagh@digiplusit.com>
2025-06-16 08:40:37 +0530

    layout done

--- src/components/HomePage/_components/AnimatedExpertiseSection .tsx
+++ src/components/HomePage/_components/AnimatedExpertiseSection .tsx
@@ -4,12 +4,60 @@ type Props = {}
 
 const AnimatedExpertiseSection = (props: Props) => {
     return (
-        <div className='lg:pt-[9rem] lf:pb-[6rem]'>
-            <div className='font-primary container-1280 w-layout-blockcontainer'>
+        <div className='lg:pt-[6rem] lg:pb-[4rem]'>
+            <div className='font-primary !px-20 container-1280 w-layout-blockcontainer'>
                 <h2>Unlocking the potential of your data</h2>
-                <div className='p-8 flex flex-row items-start'>
-                    <div></div>
-                    <div></div>
+                <div className='pt-8 flex flex-row items-start'>
+                    <div className='h-[80vh]'>
+                        <div className='text-[25rem] w-[14rem] h-[30rem] mr-16 relatives flex justify-center items-center'>01</div>
+                        <div className='text-[25rem] w-[14rem] h-[30rem] mr-16 relatives flex justify-center items-center'>02</div>
+                        <div className='text-[25rem] w-[14rem] h-[30rem] mr-16 relatives flex justify-center items-center'>03</div>
+                        <div className='text-[25rem] w-[14rem] h-[30rem] mr-16 relatives flex justify-center items-center'>04</div>
+                    </div>
+                    <div className='flex flex-col justify-between items-end w-[55vw] sticky top-[12rem]'>
+                        <div className='flex flex-col justify-center items-start w-full sticky top-[12rem] font-third'>
+                            <div className="h-[80vh] pt-8 flex flex-col gap-4">
+                                <h3>Data &amp; AI Governance</h3>
+                                <div className="wrapper-list-expertise">
+                                    <ul role="list" className="flex text-lg flex-col gap-3 w-list-unstyled">
+                                        <li>Implementation of Data &amp; IA Governance Framework.</li>
+                                        <li>Deployment of data catalog solutions and logic.</li>
+                                        <li>Change Management &amp; Onboarding.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="h-[80vh] pt-8 flex flex-col gap-4" >
+                                <h3>Data &amp; AI Governance</h3>
+                                <div className="wrapper-list-expertise">
+                                    <ul role="list" className="flex text-lg flex-col gap-3 w-list-unstyled">
+                                        <li>Implementation of Data &amp; IA Governance Framework.</li>
+                                        <li>Deployment of data catalog solutions and logic.</li>
+                                        <li>Change Management &amp; Onboarding.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="h-[80vh] pt-8 flex flex-col gap-4" >
+                                <h3>Data &amp; AI Governance</h3>
+                                <div className="wrapper-list-expertise">
+                                    <ul role="list" className="flex text-lg flex-col gap-3 w-list-unstyled">
+                                        <li>Implementation of Data &amp; IA Governance Framework.</li>
+                                        <li>Deployment of data catalog solutions and logic.</li>
+                                        <li>Change Management &amp; Onboarding.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="h-[80vh] pt-8 flex flex-col gap-4" >
+                                <h3>Data &amp; AI Governance</h3>
+                                <div className="wrapper-list-expertise">
+                                    <ul role="list" className="flex text-lg flex-col gap-3 w-list-unstyled">
+                                        <li>Implementation of Data &amp; IA Governance Framework.</li>
+                                        <li>Deployment of data catalog solutions and logic.</li>
+                                        <li>Change Management &amp; Onboarding.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
